7d4b710724c3a7a1585c24d693589e1ed820dd07
OldSerpskiStalker <51358247+oldSerpskiStalker@users.noreply.github.com>
2020-09-02 14:13:12 +0500

    * Исправил глюк с камерой

--- src/xrEngine/CameraManager.cpp
+++ src/xrEngine/CameraManager.cpp
@@ -204,7 +204,7 @@ void CCameraManager::UpdateDeffered()
         RemoveCamEffector((*it)->eType);
 
         if ((*it)->AbsolutePositioning())
-            m_EffectorsCam.insert(m_EffectorsCam.begin(), *it);
+            m_EffectorsCam.push_front(*it);
         else
             m_EffectorsCam.push_back(*it);
     }
@@ -354,8 +354,23 @@ bool CCameraManager::ProcessCameraEffector(CEffectorCam* eff)
 void CCameraManager::UpdateCamEffectors()
 {
     if (m_EffectorsCam.empty()) return;
-	for (int i = m_EffectorsCam.size() - 1; i >= 0; --i)
-		ProcessCameraEffector(m_EffectorsCam[i]);
+    EffectorCamVec::reverse_iterator r_it = m_EffectorsCam.rbegin();
+    while (r_it != m_EffectorsCam.rend())
+    {
+        if (ProcessCameraEffector(*r_it))
+        {
+            ++r_it;
+        }
+        else
+        {
+            // Dereferencing reverse iterator returns previous element of the list, r_it.base() returns current element
+            // So, we should use base()-1 iterator to delete just processed element. 'Previous' element would be 
+            // automatically changed after deletion, so r_it would dereferencing to another value, no need to change it
+            OnEffectorReleased(*r_it);
+            auto r_to_del = r_it.base();
+            m_EffectorsCam.erase(--r_to_del);
+        }
+    }
 
     m_cam_info.d.normalize();
     m_cam_info.n.normalize();
