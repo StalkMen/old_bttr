a4b512729fcdb0cf2decf6bfa0630de2483bd5de
OldSerpskiStalker <51358247+oldSerpskiStalker@users.noreply.github.com>
2020-09-05 13:45:32 +0500

    * Удалены дубликаты файлов, настройка DX10 [+]

--- src/Layers/xrRenderPC_R3/stdafx.h
+++ src/Layers/xrRenderPC_R3/stdafx.h
@@ -46,15 +46,11 @@
 #include "../xrRender/blenders\blender.h"
 #include "../xrRender/blenders\blender_clsid.h"
 #include "../xrRender/xrRender_console.h"
-#include "r3.h"
+#include "dx10.h"
 #include "../../build_render_config.h"
 
 IC	void	jitter(CBlender_Compile& C)
 {
-//	C.r_Sampler	("jitter0",	JITTER(0), true, D3DTADDRESS_WRAP, D3DTEXF_POINT, D3DTEXF_NONE, D3DTEXF_POINT);
-//	C.r_Sampler	("jitter1",	JITTER(1), true, D3DTADDRESS_WRAP, D3DTEXF_POINT, D3DTEXF_NONE, D3DTEXF_POINT);
-//	C.r_Sampler	("jitter2",	JITTER(2), true, D3DTADDRESS_WRAP, D3DTEXF_POINT, D3DTEXF_NONE, D3DTEXF_POINT);
-//	C.r_Sampler	("jitter3",	JITTER(3), true, D3DTADDRESS_WRAP, D3DTEXF_POINT, D3DTEXF_NONE, D3DTEXF_POINT);
 	C.r_dx10Texture	("jitter0",	JITTER(0));
 	C.r_dx10Texture	("jitter1",	JITTER(1));
 	C.r_dx10Texture	("jitter2",	JITTER(2));
