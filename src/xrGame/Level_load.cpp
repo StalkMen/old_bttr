bd79fd94786c3ac2b74a14067a3bd8228504aab4
OldSerpskiStalker <51358247+oldSerpskiStalker@users.noreply.github.com>
2020-09-17 00:20:49 +0500

    Фиксы hudjust, камеры шота, многопоток HOM

--- src/xrGame/Level_load.cpp
+++ src/xrGame/Level_load.cpp
@@ -114,11 +114,7 @@ bool CLevel::Load_GameSpecific_After()
 			Sounds_Random_Enabled	= FALSE;
 		}
 
-        if (g_pGamePersistent->pEnvironment) {
-            if (CEffect_Rain* rain = g_pGamePersistent->pEnvironment->eff_Rain) {
-                rain->InvalidateState();
-            }
-        }
+       g_pGamePersistent->pEnvironment->Invalidate();
 
 		if ( FS.exist(fn_game, "$level$", "level.fog_vol")) 
 		{
