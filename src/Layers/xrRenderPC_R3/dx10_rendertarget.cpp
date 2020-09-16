e05697355a7268c5cce04e395e606f12a06e4d9c
OldSerpskiStalker <51358247+oldSerpskiStalker@users.noreply.github.com>
2020-09-16 15:30:26 +0500

    Лучи солнца DX10 (Break), LV памагите

--- src/Layers/xrRenderPC_R3/dx10_rendertarget.cpp
+++ src/Layers/xrRenderPC_R3/dx10_rendertarget.cpp
@@ -10,6 +10,7 @@
 #include "../xrRenderPC_R3/blenders_DX10/blender_bloom_build.h"
 #include "../xrRenderPC_R3/blenders_DX10/blender_luminance.h"
 #include "../xrRenderPC_R3/blenders_DX10/blender_ssao.h"
+#include "blender_ss_sunshafts.h"
 #include "dx10MinMaxSMBlender.h"
 #include "../xrRenderDX10/msaa/dx10MSAABlender.h"
 #include "../xrRenderDX10/DX10 Rain/dx10RainBlender.h"
@@ -314,6 +315,7 @@ CRenderTarget::CRenderTarget		()
 	b_luminance				= xr_new<CBlender_luminance>			();
 	b_combine				= xr_new<CBlender_combine>				();
 	b_ssao					= xr_new<CBlender_SSAO_noMSAA>			();
+	b_sunshafts				= xr_new<CBlender_sunshafts>			();
 
 	if( RImplementation.o.dx10_msaa )
 	{
@@ -392,6 +394,11 @@ CRenderTarget::CRenderTarget		()
 		// generic(LDR) RTs
 		rt_Generic_0.create		(r2_RT_generic0,w,h,D3DFMT_A8R8G8B8, 1		);
 		rt_Generic_1.create		(r2_RT_generic1,w,h,D3DFMT_A8R8G8B8, 1		);
+		
+		// RT - KD
+		rt_sunshafts_0.create(r2_RT_sunshafts0, w, h, D3DFMT_A8R8G8B8);
+		rt_sunshafts_1.create(r2_RT_sunshafts1, w, h, D3DFMT_A8R8G8B8);
+		
 		if( RImplementation.o.dx10_msaa )
 		{
 			rt_Generic_0_r.create(r2_RT_generic0_r,w,h,D3DFMT_A8R8G8B8, SampleCount	);
@@ -404,7 +411,9 @@ CRenderTarget::CRenderTarget		()
 		if (RImplementation.o.advancedpp)
 			rt_Generic_2.create			(r2_RT_generic2,w,h,D3DFMT_A16B16G16R16F, SampleCount );
 	}
-
+	
+	s_sunshafts.create(b_sunshafts, "r2\\sunshafts");
+	
 	// OCCLUSION
 	s_occq.create					(b_occq,		"r2\\occq");
 
@@ -671,7 +680,10 @@ CRenderTarget::CRenderTarget		()
 
 		u32 fvf_aa_AA				= D3DFVF_XYZRHW|D3DFVF_TEX7|D3DFVF_TEXCOORDSIZE2(0)|D3DFVF_TEXCOORDSIZE2(1)|D3DFVF_TEXCOORDSIZE2(2)|D3DFVF_TEXCOORDSIZE2(3)|D3DFVF_TEXCOORDSIZE2(4)|D3DFVF_TEXCOORDSIZE4(5)|D3DFVF_TEXCOORDSIZE4(6);
 		g_aa_AA.create				(fvf_aa_AA,		RCache.Vertex.Buffer(), RCache.QuadIB);
-
+		
+		u32 fvf_KD 					= D3DFVF_XYZRHW | D3DFVF_TEX1 | D3DFVF_TEXCOORDSIZE2(0);
+		g_KD.create					(fvf_KD, RCache.Vertex.Buffer(), RCache.QuadIB);
+		
 		t_envmap_0.create			(r2_T_envs0);
 		t_envmap_1.create			(r2_T_envs1);
 	}
@@ -1016,6 +1028,7 @@ CRenderTarget::~CRenderTarget	()
 	xr_delete					(b_accum_point			);
 	xr_delete					(b_accum_direct			);
 	xr_delete					(b_ssao					);
+	xr_delete					(b_sunshafts			);
 
    if( RImplementation.o.dx10_msaa )
    {
