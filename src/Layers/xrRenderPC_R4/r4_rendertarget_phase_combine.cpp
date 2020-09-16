24fba39e1cf469b6bc8df20278d75ace097c9951
OldSerpskiStalker <51358247+oldSerpskiStalker@users.noreply.github.com>
2020-09-16 23:26:42 +0500

    Доделал лучи на DX11

--- src/Layers/xrRenderPC_R4/r4_rendertarget_phase_combine.cpp
+++ src/Layers/xrRenderPC_R4/r4_rendertarget_phase_combine.cpp
@@ -332,7 +332,12 @@ void	CRenderTarget::phase_combine	()
          HW.pDevice->ResolveSubresource( rt_Generic_1_r->pTexture->surface_get(), 0, rt_Generic_1->pTexture->surface_get(), 0, DXGI_FORMAT_R8G8B8A8_UNORM );
    }
    */
+	
+	RCache.set_Stencil(FALSE);
 
+	if (ps_r_sun_shafts > 0 && ps_sunshafts_mode == R2SS_SCREEN_SPACE)
+		phase_sunshafts();
+	
 	// PP enabled ?
 	//	Render to RT texture to be able to copy RT even in windowed mode.
 	BOOL	PP_Complex		= u_need_PP	() | (BOOL)RImplementation.m_bMakeAsyncSS;
