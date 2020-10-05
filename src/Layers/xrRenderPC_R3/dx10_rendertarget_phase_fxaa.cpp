cc75f2445cc350e281db69536e9a6ea787be8bcd
LVutner <kkamil724@gmail.com>
2020-10-06 00:28:59 +0200

    * UPD: [Render] Updated FXAA implementation Credits: RainbowZerg

--- src/Layers/xrRenderPC_R3/dx10_rendertarget_phase_fxaa.cpp
+++ src/Layers/xrRenderPC_R3/dx10_rendertarget_phase_fxaa.cpp
@@ -1,26 +1,70 @@
 #include "stdafx.h"
-#include "dx10_rendertarget.h"
 
 void CRenderTarget::phase_fxaa()
 {
+	//Constants
 	u32 Offset = 0;
-	float _w = float(Device.dwWidth);
-	float _h = float(Device.dwHeight);
-	float du = ps_r1_pps_u, dv = ps_r1_pps_v;
+	constexpr float d_Z = EPS_S;
+	constexpr float d_W = 1.0f;
+	const float w = float(Device.dwWidth);
+	const float h = float(Device.dwHeight);		
+	constexpr u32 C = color_rgba(0, 0, 0, 255);
+	
+	//////////////////////////////////////////////////////////////////////////
+	//Set MSAA/NonMSAA rendertarget
+	u_setrt(rt_fxaa_luma, nullptr, nullptr, nullptr); //No ZB
 
-	ref_rt& dest_rt = RImplementation.o.dx10_msaa ? rt_Generic : rt_Color;
-	u_setrt(dest_rt, 0, NULL, HW.pBaseZB);
+	RCache.set_CullMode(CULL_NONE);
+	RCache.set_Stencil(FALSE);
 
-	FVF::V* pv = (FVF::V*)	RCache.Vertex.Lock(4, g_fxaa->vb_stride, Offset);
-	pv->set(du + 0, dv + float(_h), 0, 0, 1);	pv++;
-	pv->set(du + 0, dv + 0, 0, 0, 0);	pv++;
-	pv->set(du + float(_w), dv + float(_h), 0, 1, 1);	pv++;
-	pv->set(du + float(_w), dv + 0, 0, 1, 0);	pv++;
-	RCache.Vertex.Unlock(4, g_fxaa->vb_stride);
+	// Half-pixel offset (DX9 only)
+	constexpr Fvector2 p0{ 0.0f, 0.0f }, p1{ 1.0f, 1.0f };
 
+	//Fill vertex buffer
+	FVF::TL* pv = (FVF::TL*)RCache.Vertex.Lock(4, g_combine->vb_stride, Offset);
+	pv->set(0, float(h), d_Z, d_W, C, p0.x, p1.y); pv++;
+	pv->set(0, 0, d_Z, d_W, C, p0.x, p0.y); pv++;
+	pv->set(float(w), float(h), d_Z, d_W, C, p1.x, p1.y); pv++;
+	pv->set(float(w), 0, d_Z, d_W, C, p1.x, p0.y); pv++;
+	RCache.Vertex.Unlock(4, g_combine->vb_stride);
+
+	//Set pass
 	RCache.set_Element(s_fxaa->E[0]);
-	RCache.set_Geometry(g_fxaa);
-	RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 4, 0, 2);
 
-	HW.pContext->CopyResource(rt_Generic_0->pTexture->surface_get(), dest_rt->pTexture->surface_get());
+	//Set paramterers
+	//RCache.set_c("taa_params", ps_taa_params.x, ps_taa_params.y, 0, 0);
+
+	//Set geometry
+	RCache.set_Geometry(g_combine);
+	RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 4, 0, 2);
+	////////////////////////////////////////////////////////////////////////////
+	ref_rt& dest_rt = RImplementation.o.dx10_msaa ? rt_Generic : rt_Color;
+	u_setrt(dest_rt, nullptr, nullptr, HW.pBaseZB);
+
+	RCache.set_CullMode(CULL_NONE);
+	RCache.set_Stencil(FALSE);
+
+	// Half-pixel offset (DX9 only)
+	constexpr Fvector2 p0{ 0.0f, 0.0f }, p1{ 1.0f, 1.0f };
+
+	//Fill vertex buffer
+	pv = (FVF::TL*)RCache.Vertex.Lock(4, g_combine->vb_stride, Offset);
+	pv->set(0, float(h), d_Z, d_W, C, p0.x, p1.y); pv++;
+	pv->set(0, 0, d_Z, d_W, C, p0.x, p0.y); pv++;
+	pv->set(float(w), float(h), d_Z, d_W, C, p1.x, p1.y); pv++;
+	pv->set(float(w), 0, d_Z, d_W, C, p1.x, p0.y); pv++;
+	RCache.Vertex.Unlock(4, g_combine->vb_stride);
+
+	//Set pass
+	RCache.set_Element(s_fxaa->E[1]);
+
+	//Set geometry
+	RCache.set_Geometry(g_combine);
+	RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 4, 0, 2);
+	
+	//Resolve RT
+	HW.pContext->CopyResource(rt_Generic_0->pTexture->surface_get(), dest_rt->pTexture->surface_get());			
 }
+
+
+
