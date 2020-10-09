24a92cb1d16232f9ef2cd9dcd25ef57a9a320ebf
OldSerpskiStalker <51358247+oldSerpskiStalker@users.noreply.github.com>
2020-10-09 16:17:22 +0500

    Передача ID уровня в шейдеры, часть 1

---
+++ src/xrEngine/ShadersLevelID.cpp
@@ -0,0 +1,8 @@
+//OldSerpskiStalker
+#include "stdafx.h"
+#include "ShadersLevelID.h"
+
+void GetLevelID::_LevelID()
+{
+	
+}
---
+++ src/xrEngine/ShadersLevelID.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "x_ray.h"
+#include "IGame_Level.h"
+
+class ENGINE_API GetLevelID
+{
+	void _LevelID();
+};
