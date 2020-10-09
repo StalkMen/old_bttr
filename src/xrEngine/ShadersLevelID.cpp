9e0e7cf3d0add075dba62992f160187160befc41
OldSerpskiStalker <51358247+oldSerpskiStalker@users.noreply.github.com>
2020-10-09 21:23:24 +0500

    Сделаю по другому

--- src/xrEngine/ShadersLevelID.cpp
+++
@@ -1,8 +0,0 @@
-//OldSerpskiStalker
-#include "stdafx.h"
-#include "ShadersLevelID.h"
-
-void GetLevelID::_LevelID()
-{
-	
-}
--- src/xrEngine/ShadersLevelID.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "x_ray.h"
-#include "IGame_Level.h"
-
-class ENGINE_API GetLevelID
-{
-	void _LevelID();
-};
