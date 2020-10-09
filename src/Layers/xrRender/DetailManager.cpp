cef8bd70c9743a3fe329e16e41c9569179ad6e4b
LVutner <kkamil724@gmail.com>
2020-10-09 08:05:30 +0200

    * FIX [Render] Fixed grass culling mode

--- src/Layers/xrRender/DetailManager.cpp
+++ src/Layers/xrRender/DetailManager.cpp
@@ -406,11 +406,14 @@ void CDetailManager::Render	()
 #endif
 	swing_current.lerp		(swing_desc[0],swing_desc[1],factor);
 
-	RCache.set_CullMode		(CULL_NONE);
+	RCache.set_CullMode		(CULL_CCW); //LV: Set cull mode only once
 	RCache.set_xform_world	(Fidentity);
-	if (UseVS())			hw_Render	();
-	else					soft_Render	();
-	RCache.set_CullMode		(CULL_CCW);
+	
+	if (UseVS())			
+		hw_Render	();
+	else					
+		soft_Render	();
+
 	g_pGamePersistent->m_pGShaderConstants->m_blender_mode.w = 0.0f; //--#SM+#--
 	RDEVICE.Statistic->RenderDUMP_DT_Render.End	();
 	m_frame_rendered		= RDEVICE.dwFrame;
