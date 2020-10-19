efbe9743b602e401a153ea0eca04bdeebcb8986d
LVutner <kkamil724@gmail.com>
2020-10-19 22:46:32 +0200

    * FIX: [xrGame] Fixed weapon "lags" - Thanks to @Mortan

--- src/xrGame/Weapon.cpp
+++ src/xrGame/Weapon.cpp
@@ -264,7 +264,10 @@ void CWeapon::UpdateXForm()
     if ((HandDependence() == hd1Hand) || (GetState() == eReload) || (!E->g_Alive()))
         boneL = boneR2;
 
-    V->CalculateBones();
+	//Fix for laggy weapon by Mortan
+	V->CalculateBones_Invalidate();
+	V->CalculateBones(true);
+	
     Fmatrix& mL = V->LL_GetTransform(u16(boneL));
     Fmatrix& mR = V->LL_GetTransform(u16(boneR));
     // Calculate
