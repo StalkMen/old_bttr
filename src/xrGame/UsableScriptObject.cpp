c3e2d4edadcf268ae3d3e9e516ea7b61d36b06d1
OldSerpskiStalker <51358247+oldSerpskiStalker@users.noreply.github.com>
2020-10-28 15:58:13 +0500

    Анимация камеры открытия дверей

--- src/xrGame/UsableScriptObject.cpp
+++ src/xrGame/UsableScriptObject.cpp
@@ -19,15 +19,29 @@ CUsableScriptObject::~CUsableScriptObject()
 {
 }
 
+#include "actor.h"
 bool CUsableScriptObject::use(CGameObject* who_use)
 {
 	VERIFY(who_use);
-	CGameObject* pThis = smart_cast<CGameObject*>(this); VERIFY(pThis);
-	
-	if ( pThis->lua_game_object() && pThis->lua_game_object()->m_door && (pThis->lua_game_object()->m_door->is_blocked(doors::door_state_open) || pThis->lua_game_object()->m_door->is_blocked(doors::door_state_closed)) )
+	CGameObject* pThis = smart_cast<CGameObject*>(this); 
+
+	VERIFY(pThis);
+
+	if (pThis->lua_game_object() && pThis->lua_game_object()->m_door && (pThis->lua_game_object()->m_door->is_blocked(doors::door_state_open) || pThis->lua_game_object()->m_door->is_blocked(doors::door_state_closed)))
 		return false;
 
-	pThis->callback(GameObject::eUseObject)(pThis->lua_game_object(),who_use->lua_game_object());
+	if (pThis && pThis->lua_game_object()->m_door)
+	{ 
+		if (Actor())
+		{
+			LPCSTR animation_door;
+			LUA_EXPORT m_functor;
+			R_ASSERT(ai().script_engine().functor("_export_touch_of_ray.animation_door_open", m_functor));
+			animation_door = m_functor();
+		}
+	}
+
+	pThis->callback(GameObject::eUseObject)(pThis->lua_game_object(), who_use->lua_game_object());
 
 	return true;
 }
