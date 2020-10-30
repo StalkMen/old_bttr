1ec1a00f6aa51505b03501065e4fc0013dddc860
OldSerpskiStalker <51358247+oldSerpskiStalker@users.noreply.github.com>
2020-10-31 01:55:40 +0500

    Исправил рендер гамму HLSL

--- src/Layers/xrRender_DXTargets/Blenders/blender_gamma.cpp
+++ src/Layers/xrRender_DXTargets/Blenders/blender_gamma.cpp
@@ -9,11 +9,11 @@ void CBlender_gamma::Compile(CBlender_Compile& C)
 {
 	IBlender::Compile(C);
 
-	C.r_Pass("stub_screen_space", "render_gamma", FALSE, FALSE, FALSE);
+	C.r_Pass("stub_notransform_aa_AA", "render_gamma", FALSE, FALSE, FALSE);
 	C.r_dx10Texture("s_image", r2_RT_generic0);
-	
+
 	C.r_dx10Texture("s_gamma", "shaders\\gamma");
-	
-	C.r_dx10Sampler("smp_nofilter");
+
+	C.r_dx10Sampler("smp_rtlinear");
 	C.r_End();
 }
