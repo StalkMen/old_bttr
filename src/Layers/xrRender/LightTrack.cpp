6fb7f3f5aa6ce51beb109a4ee1521fb9e081c263
oldSerpskiStalker <51358247+oldSerpskiStalker@users.noreply.github.com>
2020-11-13 16:02:35 +0500

    Удаление кода R1 в LightTrack и мини фикс

--- src/Layers/xrRender/LightTrack.cpp
+++ src/Layers/xrRender/LightTrack.cpp
@@ -34,17 +34,21 @@ CROS_impl::CROS_impl	()
 	sun_value			= 0.2f;
 	sun_smooth			= 0.2f;
 
-#if RENDER!=R_R1
 	last_position.set( 0.0f, 0.0f, 0.0f );
 	ticks_to_update		= 0;
 	sky_rays_uptodate	= 0;
-#endif	// RENDER!=R_R1
 
-//#if RENDER==R_R1
 	MODE				= IRender_ObjectSpecific::TRACE_ALL											;
-//#else
-//	MODE				= IRender_ObjectSpecific::TRACE_HEMI + IRender_ObjectSpecific::TRACE_SUN	;
-//#endif
+
+	for (size_t i = 0; i < NUM_FACES; ++i)
+	{
+		hemi_cube[i] = 0.1f;
+	}
+
+	for (size_t i = 0; i < NUM_FACES; ++i)
+	{
+		hemi_cube_smooth[i] = 0.1f;
+	}
 }
 
 void	CROS_impl::add		(light* source)
@@ -217,14 +221,11 @@ void	CROS_impl::update	(IRenderable* O)
 					accum.add	( sun_ );
 	if (MODE & IRender_ObjectSpecific::TRACE_LIGHTS )	{
 		Fvector		lacc	=	{ 0,0,0 };
-#if RENDER!=R_R1
 		float hemi_cube_light[NUM_FACES] = {0,0,0,0,0,0};
-#endif
 		for (u32 lit=0; lit<lights.size(); lit++)	{
 			light*	L	=	lights[lit].source;
 			float	d	=	L->position.distance_to(position);
 			
-#if RENDER!=R_R1
 			float	a	=	(1/(L->attenuation0 + L->attenuation1*d + L->attenuation2*d*d) - d*L->falloff) *(L->flags.bStatic?1.f:2.f);
 			a	=	(a > 0) ? a : 0.0f;
 
@@ -237,15 +238,12 @@ void	CROS_impl::update	(IRenderable* O)
 						* ps_r2_dhemi_light_scale;
 			
 			accum_hemi(hemi_cube_light, dir, koef);
-#else
-			float	r	=	L->range;
-			float	a	=	clampr(1.f - d/(r+EPS),0.f,1.f)*(L->flags.bStatic?1.f:2.f);
-#endif
+
 			lacc.x		+=	lights[lit].color.r*a;
 			lacc.y		+=	lights[lit].color.g*a;
 			lacc.z		+=	lights[lit].color.b*a;
 		}
-#if RENDER!=R_R1
+
 		const float	minHemiValue = 1/255.f;
 
 		float	hemi_light = (lacc.x + lacc.y + lacc.z)/3.0f * ps_r2_dhemi_light_scale;
@@ -258,7 +256,6 @@ void	CROS_impl::update	(IRenderable* O)
 			hemi_cube[i] += hemi_cube_light[i]*(1-ps_r2_dhemi_light_flow) + ps_r2_dhemi_light_flow*hemi_cube_light[(i+NUM_FACES/2)%NUM_FACES];
 			hemi_cube[i] = std::max(hemi_cube[i], minHemiValue);
 		}
-#endif
 
 //		lacc.x		*= desc.lmap_color.x;
 //		lacc.y		*= desc.lmap_color.y;
@@ -278,8 +275,6 @@ void	CROS_impl::update	(IRenderable* O)
 	update_smooth			()	;
 	approximate				=	accum;
 }
-
-#if RENDER!=R_R1
 
 //	Update ticks settings
 static const s32 s_iUTFirstTimeMin = 1;
@@ -330,8 +325,6 @@ void 	CROS_impl::smart_update(IRenderable* O)
 	}
 }
 
-#endif	//	#if RENDER!=R_R1		
-
 extern float ps_r2_lt_smooth;
 
 // hemi & sun: update and smooth
@@ -342,12 +335,7 @@ void	CROS_impl::update_smooth	(IRenderable* O)
 
 	dwFrameSmooth			=	Device.dwFrame;
 
-#if RENDER==R_R1
-	if (O && (0==result_count)) 
-		update(O)						;	// First time only
-#else	//	RENDER!=R_R1
 	smart_update(O);
-#endif	//	RENDER!=R_R1
 
 	float	l_f				=	Device.fTimeDelta*ps_r2_lt_smooth;
 	clamp	(l_f,0.f,1.f)	;
@@ -362,12 +350,7 @@ void	CROS_impl::update_smooth	(IRenderable* O)
 
 void CROS_impl::calc_sun_value(Fvector& position, CObject* _object)
 {
-
-#if RENDER==R_R1
-	light*	sun		=		(light*)RImplementation.L_DB->sun_adapted._get()	;
-#else
 	light*	sun		=		(light*)RImplementation.Lights.sun_adapted._get()	;
-#endif
 	if	(MODE & IRender_ObjectSpecific::TRACE_SUN)	{
 		if  (--result_sun	< 0)	{
 			result_sun		+=		::Random.randI(lt_hemisamples/4,lt_hemisamples/2)	;
@@ -382,11 +365,8 @@ void CROS_impl::calc_sky_hemi_value(Fvector& position, CObject* _object)
 	// hemi-tracing
 	if	(MODE & IRender_ObjectSpecific::TRACE_HEMI)	
 	{
-	
-#if RENDER!=R_R1
 		sky_rays_uptodate	+= ps_r2_dhemi_count;
 		sky_rays_uptodate	= _min(sky_rays_uptodate, lt_hemisamples);
-#endif	//	RENDER!=R_R1
 
 		for (u32 it=0; it<(u32)ps_r2_dhemi_count;	it++)		{	// five samples per one frame
 			u32	sample		=	0				;
@@ -419,93 +399,71 @@ void CROS_impl::calc_sky_hemi_value(Fvector& position, CObject* _object)
 
 void CROS_impl::prepare_lights(Fvector& position, IRenderable* O)
 {
-	CObject*	_object	= dynamic_cast<CObject*>	(O);
-	float	dt			=	Device.fTimeDelta;
+	CObject* _object = dynamic_cast<CObject*>	(O);
+	float	dt = Device.fTimeDelta;
 
-	vis_data &vis = O->renderable.visual->getVisData();
-	float	radius;		radius	= vis.sphere.R;
+	vis_data& vis = O->renderable.visual->getVisData();
+	float	radius;		radius = vis.sphere.R;
 	// light-tracing
-	BOOL	bTraceLights	= MODE & IRender_ObjectSpecific::TRACE_LIGHTS;
-	if		((!O->renderable_ShadowGenerate()) && (!O->renderable_ShadowReceive()))	bTraceLights = FALSE;
-	if		(bTraceLights)	{
+	BOOL	bTraceLights = MODE & IRender_ObjectSpecific::TRACE_LIGHTS;
+	if ((!O->renderable_ShadowGenerate()) && (!O->renderable_ShadowReceive()))	bTraceLights = FALSE;
+	if (bTraceLights) {
 		// Select nearest lights
-		Fvector					bb_size	=	{radius,radius,radius};
+		Fvector					bb_size = { radius,radius,radius };
 
-#if RENDER!=R_R1
-		g_SpatialSpace->q_box				(RImplementation.lstSpatial,0,STYPE_LIGHTSOURCEHEMI,position,bb_size);
-#else
-		g_SpatialSpace->q_box				(RImplementation.lstSpatial,0,STYPE_LIGHTSOURCE,position,bb_size);
-#endif
-		for (u32 o_it=0; o_it<RImplementation.lstSpatial.size(); o_it++)	{
-			ISpatial*	spatial		= RImplementation.lstSpatial[o_it];
-			light*		source		= (light*)	(spatial->dcast_Light());
-			VERIFY		(source);	// sanity check
-			float	R				= radius+source->range;
+		g_SpatialSpace->q_box(RImplementation.lstSpatial, 0, STYPE_LIGHTSOURCEHEMI, position, bb_size);
+
+		for (u32 o_it = 0; o_it < RImplementation.lstSpatial.size(); o_it++) {
+			ISpatial* spatial = RImplementation.lstSpatial[o_it];
+			light* source = (light*)(spatial->dcast_Light());
+			VERIFY(source);	// sanity check
+			float	R = radius + source->range;
 			if (position.distance_to(source->position) < R
-#if RENDER!=R_R1
-				&&source->flags.bStatic
-#endif
-				)		add	(source);
+
+				&& source->flags.bStatic
+
+				)		add(source);
 		}
 
 		// Trace visibility
-		lights.clear	();
-#if RENDER==R_R1 
-		float traceR	= radius*.5f;
-#endif
-		for (s32 id=0; id<s32(track.size()); id++)
+		lights.clear();
+
+		for (s32 id = 0; id < s32(track.size()); id++)
 		{
 			// remove untouched lights
-			xr_vector<CROS_impl::Item>::iterator I	= track.begin()+id;
-			if (I->frame_touched!=Device.dwFrame)	{ track.erase(I) ; id--	; continue ; }
+			xr_vector<CROS_impl::Item>::iterator I = track.begin() + id;
+			if (I->frame_touched != Device.dwFrame) { track.erase(I); id--; continue; }
 
 			// Trace visibility
-			Fvector				P,D;
-			float		amount	= 0;
-			light*		xrL		= I->source;
-			Fvector&	LP		= xrL->position;
-#if RENDER==R_R1
-			P.mad				(position,P.random_dir(),traceR);		// Random point inside range
-#else
+			Fvector				P, D;
+			float		amount = 0;
+			light* xrL = I->source;
+			Fvector& LP = xrL->position;
+
 			P = position;
-#endif
 
 			// point/spot
-			float	f			=	D.sub(P,LP).magnitude();
-			if (g_pGameLevel->ObjectSpace.RayTest(LP,D.div(f),f,collide::rqtStatic,&I->cache,_object))	amount -=	lt_dec;
-			else																						amount +=	lt_inc;
-			I->test				+=	amount * dt;	clamp	(I->test,-.5f,1.f);
-			I->energy			=	.9f*I->energy + .1f*I->test;
+			float	f = D.sub(P, LP).magnitude();
+			if (g_pGameLevel->ObjectSpace.RayTest(LP, D.div(f), f, collide::rqtStatic, &I->cache, _object))	amount -= lt_dec;
+			else																						amount += lt_inc;
+			I->test += amount * dt;	clamp(I->test, -.5f, 1.f);
+			I->energy = .9f * I->energy + .1f * I->test;
 
 			// 
-			float	E			=	I->energy * xrL->color.intensity	();
-			if (E > EPS)		{
+			float	E = I->energy * xrL->color.intensity();
+			if (E > EPS) {
 				// Select light
-				lights.push_back			(CROS_impl::Light())		;
-				CROS_impl::Light&	L		= lights.back()				;
-				L.source					= xrL						;
-				L.color.mul_rgb				(xrL->color,I->energy/2)	;
-				L.energy					= I->energy/2				;
-				if (!xrL->flags.bStatic)	{ L.color.mul_rgb(.5f); L.energy *= .5f; }
+				lights.push_back(CROS_impl::Light());
+				CROS_impl::Light& L = lights.back();
+				L.source = xrL;
+				L.color.mul_rgb(xrL->color, I->energy / 2);
+				L.energy = I->energy / 2;
+				if (!xrL->flags.bStatic) { L.color.mul_rgb(.5f); L.energy *= .5f; }
 			}
 		}
 
-#if RENDER==R_R1
-		light*	sun		=		(light*)RImplementation.L_DB->sun_adapted._get()	;
-
-		// Sun
-		float	E			=	sun_smooth * sun->color.intensity	();
-		if (E > EPS)		{
-			// Select light
-			lights.push_back			(CROS_impl::Light())		;
-			CROS_impl::Light&	L		= lights.back()				;
-			L.source					= sun						;
-			L.color.mul_rgb				(sun->color,sun_smooth/2)	;
-			L.energy					= sun_smooth				;
-		}
-#endif
 		// Sort lights by importance - important for R1-shadows
-		std::sort	(lights.begin(),lights.end(), pred_energy);
+		std::sort(lights.begin(), lights.end(), pred_energy);
 
 	}
 }
