774be6b09a427a170e209dceb319319193f29079
oldSerpskiStalker <51358247+oldSerpskiStalker@users.noreply.github.com>
2020-11-20 11:06:20 +0500

    Фикс пси-собаки, аура и ее воздействие в оффлайн ГГ

--- src/xrGame/ai/monsters/pseudodog/psy_dog_aura.cpp
+++ src/xrGame/ai/monsters/pseudodog/psy_dog_aura.cpp
@@ -57,17 +57,28 @@ void CPsyDogAura::reinit()
 
 void CPsyDogAura::update_schedule()
 {
-	if (!m_object->g_Alive()) 
+	if (!m_object->g_Alive())
 		return;
 
-	m_time_phantom_saw_actor	= 0;
+	if (!m_actor->g_Alive())
+	{
+		if (active())
+		{
+			m_effector->switch_off();
+			m_effector = 0;
+		}
+		return;
+	}
+
+	m_time_phantom_saw_actor = 0;
 
 	// check memory of actor and check memory of phantoms
 	CVisualMemoryManager::VISIBLES::const_iterator	I = m_actor->memory().visual().objects().begin();
 	CVisualMemoryManager::VISIBLES::const_iterator	E = m_actor->memory().visual().objects().end();
-	for ( ; I != E; ++I) {
-		const CGameObject *obj = (*I).m_object;
-		if (smart_cast<const CPsyDogPhantom *>(obj)) {
+	for (; I != E; ++I) {
+		const CGameObject* obj = (*I).m_object;
+		if (smart_cast<const CPsyDogPhantom*>(obj)) 
+		{
 			if (m_actor->memory().visual().visible_now(obj))
 				m_time_actor_saw_phantom = time();
 		}
@@ -75,7 +86,8 @@ void CPsyDogAura::update_schedule()
 
 	// check memory and enemy manager of phantoms whether they see actor
 	xr_vector<CPsyDogPhantom*>::iterator it = m_object->m_storage.begin();
-	for (; it !=  m_object->m_storage.end();++it) {
+	for (; it != m_object->m_storage.end(); ++it) 
+	{
 		if ((*it)->EnemyMan.get_enemy() == m_actor)
 			m_time_phantom_saw_actor = time();
 		else {
@@ -88,26 +100,31 @@ void CPsyDogAura::update_schedule()
 			}
 		}
 
-		if (m_time_phantom_saw_actor == time()) 
+		if (m_time_phantom_saw_actor == time())
 			break;
 	}
 
-	bool const close_to_actor	=	m_actor ? m_object->Position().distance_to(m_actor->Position()) < 30 : false;
-	bool const need_be_active	=	((m_time_actor_saw_phantom + 2000 > time()) || 
-									(m_time_phantom_saw_actor + 10000 > time())) && close_to_actor;
-	if (active()) {
-		if (!need_be_active) {
-			m_effector->switch_off	();
-			m_effector				= 0;
-		}
-	} else {
-		if (need_be_active) {
-			// create effector
-			m_effector = xr_new<CPPEffectorPsyDogAura>(m_state, 5000);
-			Actor()->Cameras().AddPPEffector		(m_effector);
+	bool const close_to_actor = m_actor ? m_object->Position().distance_to(m_actor->Position()) < 30 : false;
+	bool const close_to_actor = m_actor ? m_object->Position().distance_to(m_actor->Position()) < 30 : false;
+	bool const need_be_active = ((m_time_actor_saw_phantom + 2000 > time()) ||
+		(m_time_phantom_saw_actor + 10000 > time())) && close_to_actor;
+	if (active()) 
+	{
+		if (!need_be_active) 
+		{
+			m_effector->switch_off();
+			m_effector = 0;
 		}
 	}
-
+	else 
+	{
+		if (need_be_active) 
+		{
+			// create effector
+			m_effector = xr_new<CPPEffectorPsyDogAura>(m_state, 5000);
+			Actor()->Cameras().AddPPEffector(m_effector);
+		}
+	}
 }
 
 void CPsyDogAura::on_death()
