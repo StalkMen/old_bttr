73dbd6cc147a10a0865598888131e5391989d4de
oldSerpskiStalker <51358247+oldSerpskiStalker@users.noreply.github.com>
2020-11-20 11:07:26 +0500

    Фикс слышимости мутантов оружия с глушителем

--- src/xrGame/ai/monsters/basemonster/base_monster_feel.cpp
+++ src/xrGame/ai/monsters/basemonster/base_monster_feel.cpp
@@ -55,8 +55,10 @@ void CBaseMonster::feel_sound_new(CObject* who, int eType, CSound_UserDataPtr us
 		SoundMemory.check_help_sound(eType, entity->ai_location().level_vertex_id());
 		return;
 	}
-	
-	if ((eType & SOUND_TYPE_WEAPON_SHOOTING) == SOUND_TYPE_WEAPON_SHOOTING) power = 1.f;
+
+#pragma todo("OldSerpskiStalker. OGSR fix, мутанты теперь по разному слышат звуки оружия с глушителем и без него")
+//	if ((eType & SOUND_TYPE_WEAPON_SHOOTING) == SOUND_TYPE_WEAPON_SHOOTING) 
+//		power = 1.f;
 
 	if (((eType & SOUND_TYPE_WEAPON_BULLET_HIT) == SOUND_TYPE_WEAPON_BULLET_HIT) && (dist < 2.f)) 
 		HitMemory.add_hit(who,eSideFront);
