ae34115c7c25992ad7b7562d6d2c0f2f7661d2b5
LVutner <kkamil724@gmail.com>
2021-01-10 17:57:48 +0100

    * ADD: [Render] Added albedo sampler for indirect light

--- src/Layers/xrRender_DXTargets/Blenders/blender_light_reflected.cpp
+++ src/Layers/xrRender_DXTargets/Blenders/blender_light_reflected.cpp
@@ -13,7 +13,7 @@ void BLENDER::CBlender_accum_reflected::Compile(CBlender_Compile& C)
 	C.r_Pass			("accum_volume",	"accum_indirect_nomsaa",false,	FALSE,FALSE,blend,D3DBLEND_ONE,dest);
 	
 	C.r_dx10Texture		("s_position",		r2_RT_P);
-
+	C.r_dx10Texture		("s_diffuse",		r2_RT_albedo);	
 	C.r_dx10Sampler		("smp_nofilter");
 	C.r_dx10Sampler		("smp_material");
 	C.r_End				();
@@ -37,7 +37,7 @@ void BLENDER::CBlender_accum_reflected_msaa::Compile(CBlender_Compile& C)
 	C.r_Pass			("accum_volume",	"accum_indirect_msaa",false,	FALSE,FALSE,blend,D3DBLEND_ONE,dest);
 	
 	C.r_dx10Texture		("s_position",		r2_RT_P);
-
+	C.r_dx10Texture		("s_diffuse",		r2_RT_albedo);	
 	C.r_dx10Sampler		("smp_nofilter");
 	C.r_dx10Sampler		("smp_material");
 	C.r_End				();
