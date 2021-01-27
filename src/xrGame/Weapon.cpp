38f52d37063254d8057110b97aba52a21499763e
oldSerpskiStalker <51358247+oldSerpskiStalker@users.noreply.github.com>
2021-01-27 16:37:18 +0500

    Правка прицеливания (Проверить)

--- src/xrGame/Weapon.cpp
+++ src/xrGame/Weapon.cpp
@@ -2204,22 +2204,11 @@ void CWeapon::UpdateHudAdditonal(Fmatrix& trans)
 		hud_rotation.translate_over(curr_offs);
 		trans.mulB_43(hud_rotation);
 
-        float AimSpeed = m_zoom_params.m_fZoomRotateTime;  // 8.f normal
-
-        if (pActor->IsZoomAimingMode())
-        {
-            const float targetHeight = 1.1f;
-            const float dti = AimSpeed * Device.dwTimeDelta / 1000.0f;
-            m_zoom_params.m_fZoomRotationFactor = (m_zoom_params.m_fZoomRotationFactor * (1.0f - dti)) + (targetHeight * dti);
-        }
-        else
-        {
-            const float targetHeight = -0.1f;
-            const float dti = AimSpeed * Device.dwTimeDelta / 1000.0f;
-            m_zoom_params.m_fZoomRotationFactor = (m_zoom_params.m_fZoomRotationFactor * (1.0f - dti)) + (targetHeight * dti);
-        }
-
-		clamp(m_zoom_params.m_fZoomRotationFactor, 0.f, 1.f);
+        float src = 10 * Device.fTimeDelta * 0.65;
+        float target = pActor->IsZoomAimingMode() ? 1.3f : -0.3f;
+        clamp(src, 0.f, 1.f);
+        m_zoom_params.m_fZoomRotationFactor = _lerp(m_zoom_params.m_fZoomRotationFactor, target, src);
+        clamp(m_zoom_params.m_fZoomRotationFactor, 0.f, 1.0f);
 	}
 
     //============= Iiaaioaaeeaaai iauea ia?aiaiiua =============//
